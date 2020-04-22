acfbc8de246c74815713d4cdecf7d066472afd06
coding-fantastic <alexnzioka@protonmail.com>
2020-04-23 00:27:28 +0300

    include database and object file

--- delete_product.php
+++ delete_product.php
@@ -2,7 +2,9 @@
 // check if value was posted
 if($_POST){
 
-    
+  // include database and object file
+  include_once 'config/database.php';
+  include_once 'objects/product.php';
 
     // set product id to be deleted
     $product->id = $_POST['object_id'];
